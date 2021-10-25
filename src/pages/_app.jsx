32d47a749c612d0f85619b51181a36cf9f6d3b3d
Kieren Funk <kierenfunk@gmail.com>
2021-10-25 12:52:24 +0200

    init

---
+++ src/pages/_app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'normalize.css';
+import 'tailwindcss/tailwind.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;

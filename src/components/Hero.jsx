c76d71fcfb8a834f135472ba1e3e6a7d649abd5b
Kieren Funk <kierenfunk@gmail.com>
2021-11-20 19:33:33 +0100

    linting fix, next scripts for build and export

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Nav from '../components/Nav';
+import Nav from './Nav';
 
 const Hero = () => (
-    <div className="h-screen bg-hero-image bg-right bg-cover flex flex-col px-8 md:px-20">
-        <Nav/>
-        <div className="md:h-96 flex items-center flex-grow text-gray-800">
-            <div className="flex flex-col justify-center gap-4 max-w-xl">
-                <h1 className="text-5xl font-bold">Make your broker business better.</h1>
-                <p className="text-xl">We help mortgage brokers automate processes, 
-                create beautiful user experiences and use data to make better decisions.</p>
-            </div>
-        </div>
+  <div className="h-screen bg-hero-image bg-right bg-cover flex flex-col px-8 md:px-20">
+    <Nav />
+    <div className="md:h-96 flex items-center flex-grow text-gray-800">
+      <div className="flex flex-col justify-center gap-4 max-w-xl">
+        <h1 className="text-5xl font-bold">Make your broker business better.</h1>
+        <p className="text-xl">
+          We help mortgage brokers automate processes,
+          create beautiful user experiences and use data to make better decisions.
+        </p>
+      </div>
     </div>
-)
+  </div>
+);
 
 export default Hero;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Nav = () => (
-    <div className="sticky py-8 flex flex-row justify-between items-center z-20">
-        <div className="">
-            <img src="logo.png" alt="Fineo Digital Logo"></img>
-        </div>
-        <div className="flex flex-row gap-16 items-center uppercase tracking-wider text-sm font-bold text-gray-600">
-            <div className="">Connect</div>
-        </div>
+  <div className="sticky py-8 flex flex-row justify-between items-center z-20">
+    <div className="">
+      <img src="logo.png" alt="Fineo Digital Logo" />
     </div>
-)
+    <div className="flex flex-row gap-16 items-center uppercase tracking-wider text-sm font-bold text-gray-600">
+      <div className="">Connect</div>
+    </div>
+  </div>
+);
 
 export default Nav;
